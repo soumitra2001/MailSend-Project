9b01fefebea2ab47cc010eca90fef2961db3e646
Supriya Ghorai <supriyagro2001@gmail.com>
2023-04-15 05:39:00 +0530

    new project added

---
+++ SendMailJava/src/main/java/com/geekster/MailAuthenticator.java
@@ -0,0 +1,17 @@
+package com.geekster;
+
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class MailAuthenticator extends Authenticator {
+
+    @Override
+    protected PasswordAuthentication getPasswordAuthentication()
+    {
+
+        return new PasswordAuthentication(MailConstants.SENDER, "vqzhwelbargnbupj");
+    }
+
+
+}
---
+++ SendMailJava/src/main/java/com/geekster/Main.java
@@ -0,0 +1,14 @@
+package com.geekster;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //prepare to send mail
+
+        System.out.println("This application is supposed to send mails");
+
+        HandleMail mailer = new HandleMail();
+        mailer.sendMail();
+    }
+}
